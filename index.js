cbedcf93cf7d59b72a51ac507ba29e6a42c82845
Pablo Barrios <exepablo96@gmail.com>
2025-04-22 11:03:31 -0300

    add express

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
-const notes = [
+let notes = [
   { id: 1, content: "Soy el content", date: "2020", important: true },
   { id: 2, content: "Soy segundo content", date: "2021", important: false },
   { id: 3, content: "Soy tercer content", date: "2022", important: false },
 ];
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(notes));
+//Con HTTP
+// const app = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "application/json" });
+//   res.end(JSON.stringify(notes));
+// });
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  console.log(id);
+  const note = notes.find((n) => n.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  res.status(204).end();
 });
 
 const PORT = 3001;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
-console.log(`Server running on port ${PORT}`);
+//next step instalar express
