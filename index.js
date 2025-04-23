1dd2d6f12b1a809be618231a543bc218f14ad361
Pablo Barrios <exepablo96@gmail.com>
2025-04-22 22:23:21 -0300

    create post endoint

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+//parsea lo que se manda en la request para tenerlo en el body
+app.use(express.json());
 
 let notes = [
   { id: 1, content: "Soy el content", date: "2020", important: true },
@@ -37,9 +39,28 @@ app.delete("/api/notes/:id", (req, res) => {
   res.status(204).end();
 });
 
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+  if (!note || !note.content) {
+    return res.status(400).json({
+      error: "note.content is missing",
+    });
+  }
+  const ids = notes.map((note) => note.id);
+  const maxId = Math.max(...ids);
+
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof note.important !== "undefined" ? note.important : false,
+    date: new Date().toISOString(),
+  };
+  notes = [...notes, newNote];
+
+  res.status(201).json(newNote);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-//next step instalar express
