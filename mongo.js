03789d33da3f40d327e7cfa1aa2da1339a9df18b
Pablo Barrios <exepablo96@gmail.com>
2025-04-30 11:04:40 -0300

    remove note model

--- mongo.js
+++ mongo.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const password = require("./password.js");
-const { model, Schema } = mongoose;
 
-const uri = `mongodb+srv://Pablinho:${password}@fullstack.ptapihp.mongodb.net/?retryWrites=true&w=majority&appName=FullStack`;
+const uri = `mongodb+srv://Pablinho:${password}@fullstack.ptapihp.mongodb.net/mongoDB?retryWrites=true&w=majority&appName=FullStack`;
 
+//conexion a mongodb
 mongoose
   .connect(uri)
   .then(() => {
@@ -12,35 +12,3 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
-
-const noteSchema = new Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-});
-
-const Note = model("Note", noteSchema);
-Note.find({})
-  .then((result) => {
-    console.log(result);
-    mongoose.connection.close();
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
-// const note = new Note({
-//   content: "MongoBD note from api",
-//   date: new Date(),
-//   important: true,
-// });
-
-// note
-//   .save()
-//   .then((result) => {
-//     console.log(result);
-//     mongoose.connection.close();
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
