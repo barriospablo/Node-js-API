5634cf824b818437ea16b7620171d8d5f6ab761e
Pablo Barrios <exepablo96@gmail.com>
2025-05-07 22:35:50 -0300

    update unit test

--- test/node.test.js
+++ test/node.test.js
@@ -1,14 +1,54 @@
 const supertest = require("supertest");
 const mongoose = require("mongoose");
 const { app, server } = require("../index");
+const Note = require("../models/Note");
 
 const api = supertest(app);
 
+const initialNotes = [
+  {
+    content: "Soy una nota",
+    important: true,
+    date: new Date(),
+  },
+  {
+    content: "Soy otra nota",
+    important: true,
+    date: new Date(),
+  },
+  {
+    content: "Loremmmn ipsum",
+    important: true,
+    date: new Date(),
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+
+  const note1 = new Note(initialNotes[0]);
+  await note1.save();
+
+  const note2 = new Note(initialNotes[1]);
+  await note2.save();
+});
+
+test("get all notes", async () => {
+  await api
+    .get("/api/notes")
+    .expect(200)
+    .expect("Content-type", /application\/json/);
+});
+
 test("notes are returned as json", async () => {
   const response = await api.get("/api/notes");
+  expect(response.body).toHaveLength(2);
+});
 
-  expect(response.body).toHaveLength(1);
-  // .expect("Content-type", /application\/json/);
+test(" the first note is about note", async () => {
+  const response = await api.get("/api/notes");
+  const contents = response.body.map((note) => note.content);
+  expect(contents).toContain("Soy una nota");
 });
 
 afterAll(() => {
