63e4e48d768dbd9df7e1f97d98a371f0a39c161c
Pablo Barrios <exepablo96@gmail.com>
2025-05-07 22:18:54 -0300

    add testing url environment

--- mongo.js
+++ mongo.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const password = require("./password.js");
 
-const uri = process.env.MONGO_DB_URI;
+const { MONGO_DB_URI_TEST, MONGO_DB_URI, NODE_ENV } = process.env;
+const connectionString = NODE_ENV === "test" ? MONGO_DB_URI_TEST : MONGO_DB_URI;
 
 //conexion a mongodb
 mongoose
-  .connect(uri)
+  .connect(connectionString)
   .then(() => {
     console.log("DataBase connected");
   })
