166821f4fe6fa9bb0cc9021b6de8b0e5c9594c4c
Pablo Barrios <exepablo96@gmail.com>
2025-05-09 11:06:14 -0300

    add helper file

---
+++ test/helpers.js
@@ -0,0 +1,21 @@
+const { app } = require("../index");
+const supertest = require("supertest");
+const api = supertest(app);
+const initialNotes = [
+  {
+    content: "Soy una nota",
+    important: true,
+    date: new Date(),
+  },
+  {
+    content: "Soy otra nota",
+    important: true,
+    date: new Date(),
+  },
+];
+const getAllCOntentFromNotes = async () => {
+  const response = await api.get("/api/notes");
+  return response.body.map((note) => note.content);
+};
+
+module.exports = { initialNotes, api, getAllCOntentFromNotes };
