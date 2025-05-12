658d000b2da6c3f5e88f361091ca663bd83b4ddb
Pablo Barrios <exepablo96@gmail.com>
2025-05-12 11:01:59 -0300

    add delete test

--- test/node.test.js
+++ test/node.test.js
@@ -6,11 +6,10 @@ const { initialNotes, api, getAllCOntentFromNotes } = require("./helpers");
 beforeEach(async () => {
   await Note.deleteMany({});
 
-  const note1 = new Note(initialNotes[0]);
-  await note1.save();
-
-  const note2 = new Note(initialNotes[1]);
-  await note2.save();
+  for (const note of initialNotes) {
+    const noteObject = new Note(note);
+    await noteObject.save();
+  }
 });
 
 test("get all notes", async () => {
@@ -26,8 +25,8 @@ test("notes are returned as json", async () => {
 });
 
 test("the first note is about note", async () => {
-  const contents = await getAllCOntentFromNotes();
-  expect(contents).toContain("Soy una nota");
+  const { content } = await getAllCOntentFromNotes();
+  expect(content).toContain("Soy una nota");
 });
 test("a valid  note can be added", async () => {
   const newNote = {
@@ -57,6 +56,24 @@ test("a note without content cant be added", async () => {
   const response = await api.get("/api/notes");
   const contents = response.body.map((note) => note.content);
 
+  expect(response.body).toHaveLength(initialNotes.length);
+});
+test("a note can be delelted", async () => {
+  const { response } = await getAllCOntentFromNotes();
+  const { body: notes } = response;
+  const noteToDelete = notes[0];
+
+  await api.delete(`/api/notes/${noteToDelete.id}`).expect(204);
+
+  const { content, response: secondResponse } = await getAllCOntentFromNotes();
+  expect(secondResponse.body).toHaveLength(initialNotes.length - 1);
+  expect(content).not.toContain(noteToDelete.content);
+});
+
+test("a that not exist cant be deleted", async () => {
+  await api.delete(`/api/notes/1234`).expect(400);
+
+  const { response } = await getAllCOntentFromNotes();
   expect(response.body).toHaveLength(initialNotes.length);
 });
 
