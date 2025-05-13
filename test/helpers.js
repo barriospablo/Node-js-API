cdaac19529aad96c9f7eea8ca5e0dd0562d4cd9e
Pablo Barrios <exepablo96@gmail.com>
2025-05-13 10:45:42 -0300

    add helper to get all users

--- test/helpers.js
+++ test/helpers.js
@@ -1,5 +1,6 @@
 const { app } = require("../index");
 const supertest = require("supertest");
+const User = require("../models/User");
 const api = supertest(app);
 const initialNotes = [
   {
@@ -21,5 +22,9 @@ const getAllCOntentFromNotes = async () => {
     response,
   };
 };
+const getUsers = async () => {
+  const usersDB = await User.find({});
+  return usersDB.map((user) => user.toJSON());
+};
 
-module.exports = { initialNotes, api, getAllCOntentFromNotes };
+module.exports = { initialNotes, api, getAllCOntentFromNotes, getUsers };
