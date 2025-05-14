50542b1f9e8760c9f068bf723a8ed412d9e8fe21
Pablo Barrios <exepablo96@gmail.com>
2025-05-14 10:47:32 -0300

    add test case for unique validation

--- test/user.test.js
+++ test/user.test.js
@@ -29,4 +29,24 @@ describe.only("create user", () => {
     const usersAtEnd = await getUsers();
     expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
   });
+  test("creation fails with proper status code", async () => {
+    const usersAtStart = await getUsers();
+
+    const newUser = {
+      username: "PablinhoTests",
+      name: "user",
+      password: "userpass",
+    };
+    const result = await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    expect(result.body.error.errors.username.message).toContain(
+      "`username` to be unique."
+    );
+    const usersAtEnd = await getUsers();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  });
 });
