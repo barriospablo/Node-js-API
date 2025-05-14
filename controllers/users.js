fb9309782f1b4e4d4c609b54b903a29d7843e484
Pablo Barrios <exepablo96@gmail.com>
2025-05-14 10:46:49 -0300

    add try catch to set error

--- controllers/users.js
+++ controllers/users.js
@@ -3,19 +3,23 @@ const usersRouter = require("express").Router();
 const User = require("../models/User");
 
 usersRouter.post("/", async (req, res) => {
-  const { body } = req;
-  const { username, name, password } = body;
+  try {
+    const { body } = req;
+    const { username, name, password } = body;
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(password, saltRounds);
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  });
-  const savedUser = await user.save();
-  res.status(201).json(savedUser);
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    });
+    const savedUser = await user.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 module.exports = usersRouter;
